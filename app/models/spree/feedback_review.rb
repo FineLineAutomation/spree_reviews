ccda4661151af0b131830df58438c25612fa61ea
Nathan Lowrie <nate@finelineautomation.com>
2014-01-25 22:00:50 -0500

    Fixed a missing translation error.
    Signed-off-by: Nathan Lowrie <nate@finelineautomation.com>

--- app/models/spree/feedback_review.rb
+++ app/models/spree/feedback_review.rb
@@ -3,7 +3,7 @@ class Spree::FeedbackReview < ActiveRecord::Base
   belongs_to :user, :class_name => Spree.user_class.to_s
 
   validates_presence_of :review_id
-  validates_numericality_of :rating, :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 5, :message => I18n.t('you_must_enter_value_for_rating')
+  validates_numericality_of :rating, :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 5, :message => Spree.t('you_must_enter_value_for_rating')
 
   default_scope order("spree_feedback_reviews.created_at DESC")
 
